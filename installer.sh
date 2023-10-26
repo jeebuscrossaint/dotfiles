ba1fa064ee3b4d69df707871c2bd22b70c92b619
Amarnath P. <thediamond270@gmail.com>
2023-10-26 10:50:05 -0400

    wip nvidia pkg list

--- installer.sh
+++ installer.sh
@@ -1,3 +1,4 @@
+# Created by Amarnath Patel. Dotfile installer for Arch/Arch based distros.
 echo "Welcome to the shlawg config! Would you like to (Re)install all your configurations? This installer is meant for Arch/Arch based distributions."
 confirm=""
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
@@ -14,7 +15,7 @@ if ! which paru &> /dev/null; then
 else
   echo "Paru is already installed. Continuing."
 fi
-
+# nvidia gpu check
 echo "Do you have an NVIDIA GPU? Read the text below to check if you do or do not. IF it says NVIDIA anywhere then answer yes, if not answer no. This is due to NVIDIA's drivers being closed source like a bum."
 lspci -v -s $(lspci | grep ' VGA ' | cut -d" " -f 1)
 gpuconfirm=""
