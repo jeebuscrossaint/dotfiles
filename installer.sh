8ff992e377f441b4a694941c42164eb35e815e50
Amarnath P. <thediamond270@gmail.com>
2023-10-26 07:58:25 -0400

    package lists and whatnot and better basically completed installer.

--- installer.sh
+++ installer.sh
@@ -1,18 +1,24 @@
 echo "Welcome to the shlawg config! Would you like to (Re)install all your configurations? This installer is meant for Arch/Arch based distributions."
 confirm=""
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-echo "Do you have an NVIDIA GPU? Read the text below to check if you do or do not. IF it says NVIDIA anywhere then answer yes, if not answer no. This is due to nvidia's drivers being closed source like a bum."
-lspci  -v -s  $(lspci | grep ' VGA ' | cut -d" " -f 1)
+echo "I am installing paru. A goated rust written AUR helper."
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ..
+echo "Do you have an NVIDIA GPU? Read the text below to check if you do or do not. IF it says NVIDIA anywhere then answer yes, if not answer no. This is due to NVIDIA's drivers being closed source like a bum."
+lspci -v -s $(lspci | grep ' VGA ' | cut -d" " -f 1)
 gpuconfirm=""
 read -p "Explicitly answer Y or N to the above inquiry.
 " gpuconfirm
-if [ $gpuconfirm = Y ] 
-then
-	echo "installing nvidia pkgs"
-if 
-   [ $gpuconfirm = N ]
-then
-	echo "installing normal pkgs"
+
+if [ $gpuconfirm = Y ]; then
+  echo "installing NVIDIA pkgs."
+  paru -S --needed - < nvidiapkglist.txt
+elif [ $gpuconfirm = N ]; then
+  echo "Installing normal pkgs."
+  paru -S --needed - < pkglist.txt
 else
-	echo "u dumb"
+  echo "Its a yes or no question, you bum."
 fi
