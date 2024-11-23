2cb785fb40e90bcdd3b906b19eab47833218720c
jeebuscrossaint <thediamond270@gmail.com>
2024-11-23 11:14:40 -0500

    nvim update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -527,7 +527,7 @@ require("lazy").setup({
 		end,
 	},
 
-	{
+	--[[{
 		"hrsh7th/nvim-compe",
 		event = "InsertEnter",
 		config = function()
@@ -557,7 +557,24 @@ require("lazy").setup({
 				},
 			})
 		end,
+	}, ]]
+
+	{
+		"hrsh7th/nvim-cmp"	
 	},
+
+	{
+		  "okuuva/auto-save.nvim",
+		    version = '^1.0.0', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+		      cmd = "ASToggle", -- optional for lazy loading on command
+		        event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+		          opts = {
+		          	    -- your config goes here
+		          	        -- or just leave it empty :)
+		          	          },
+		          	          },
+		     
+	
 
 	{
 		"uZer/pywal16.nvim",
