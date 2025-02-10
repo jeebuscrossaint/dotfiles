1c2ec14c454ec34f2f1c5de11b678c47e77bb5af
jeebuscrossaint <thediamond270@gmail.com>
2025-02-10 12:47:07 -0500

    stuff

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -17,7 +17,7 @@ set_animated_wallpaper_x11() {
 # Function to set wallpaper for Wayland
 set_wallpaper_wayland() {
     swww-daemon
-    export SWWW_TRANSITION=random # should be random
+    export SWWW_TRANSITION=fade # should be random
     export SWWW_TRANSITION_FPS=120
     export SWWW_TRANSITION_DURATION=5
     swww img "$1"
