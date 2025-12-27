56c0c1ad1dc312d10a017c714f1d7a41b535c6ce
jeebuscrossaint <thediamond270@gmail.com>
2025-12-27 00:20:40 -0500

    update ls

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,5 @@
 Invoke-Expression (&starship init powershell)
 fastfetch
 Set-Alias -Name jit -Value git
-Set-Alias -Name ls -Value lsd
 Set-Alias -Name cl -Value clear
 Invoke-Expression (&scoop-search --hook)
