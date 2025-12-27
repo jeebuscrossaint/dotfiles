e86fb3a8b453fa0e40ce956ba8b763663d2cd52d
jeebuscrossaint <thediamond270@gmail.com>
2025-12-27 00:17:43 -0500

    organize config a bit more

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# your system. Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running 'nixos-help').
 
 {
   config,
@@ -13,21 +13,43 @@
 
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # ============================================================================
+  # System Configuration
+  # ============================================================================
 
+  system.stateVersion = "25.11";
+  networking.hostName = "nixos";
+  time.timeZone = "America/New_York";
+
+  # ============================================================================
+  # Boot Configuration
+  # ============================================================================
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxPackages_latest;
+    tmp.useTmpfs = true;
+    plymouth.enable = true;
+  };
+
+  # ============================================================================
+  # Hardware Configuration
+  # ============================================================================
+
+  # Graphics
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
   };
 
+  # NVIDIA
   services.xserver.videoDrivers = [ "nvidia" ];
-
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
@@ -35,24 +57,23 @@
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
-
   };
 
-  programs.fish.enable = true;
-  users.users.amarnath.shell = pkgs.fish;
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  # ============================================================================
+  # Networking
+  # ============================================================================
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  networking.networkmanager.enable = true;
+  services.openssh.enable = true;
+  services.tailscale.enable = true;
 
-  systemd.settings.Manager.RebootWatchdogSec = "0";
-  systemd.settings.Manager.KExecWatchdogSec = "0";
-
-  services.fwupd.enable = true;
-
+  # Printing & Discovery
   services.printing.enable = true;
   services.avahi = {
     enable = true;
@@ -60,101 +81,11 @@
     openFirewall = true;
   };
 
-  boot = {
-    plymouth = {
-      enable = true;
-    };
-  };
+  # ============================================================================
+  # Localization
+  # ============================================================================
 
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-  };
-
-  nix.settings.auto-optimise-store = true;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-
-  system.autoUpgrade = {
-    enable = true;
-    flake = "path:/home/amarnath/dotfiles/nixos";
-    flags = [
-      "--update-input"
-      "nixpkgs"
-    ];
-    dates = "daily";
-    allowReboot = false;
-  };
-
-  boot.tmp.useTmpfs = true;
-  services.fstrim.enable = true;
-
-  services.locate = {
-    enable = true;
-    package = pkgs.plocate;
-    interval = "hourly";
-  };
-
-  nix.settings.max-jobs = "auto";
-  nix.settings.cores = 0;
-
-  programs.gamemode.enable = true;
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    gamescopeSession.enable = true;
-  };
-
-  services.getty.autologinUser = "amarnath";
-  services.flatpak.enable = true;
-  services.flatpak.packages = [
-    "org.vinegarhq.Sober"
-  ];
-  services.flatpak.update.onActivation = true;
-
-  programs.nix-ld.enable = true;
-  xdg.portal.enable = true;
-  xdg.portal.wlr.enable = true;
-  xdg.portal.config.common.default = "*";
-  security.polkit.enable = true;
-
-  services.tuned.enable = true;
-
-  zramSwap = {
-    enable = true;
-    algorithm = "zstd";
-    memoryPercent = 50;
-  };
-
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -167,16 +98,32 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # ============================================================================
+  # Audio
+  # ============================================================================
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # ============================================================================
+  # User Configuration
+  # ============================================================================
+
   users.users.amarnath = {
     isNormalUser = true;
     description = "amarnath";
+    shell = pkgs.fish;
     extraGroups = [
       "networkmanager"
       "wheel"
@@ -184,42 +131,119 @@
     packages = with pkgs; [ ];
   };
 
-  # Allow unfree packages
+  services.getty.autologinUser = "amarnath";
+
+  # ============================================================================
+  # Nix Configuration
+  # ============================================================================
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    auto-optimise-store = true;
+    max-jobs = "auto";
+    cores = 0;
+  };
+
+  # Garbage Collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  # Auto Upgrade
+  system.autoUpgrade = {
+    enable = true;
+    flake = "path:/home/amarnath/dotfiles/nixos";
+    flags = [
+      "--update-input"
+      "nixpkgs"
+    ];
+    dates = "daily";
+    allowReboot = false;
+  };
+
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # ============================================================================
+  # System Services & Optimization
+  # ============================================================================
+
+  # Systemd
+  systemd.settings.Manager = {
+    RebootWatchdogSec = "0";
+    KExecWatchdogSec = "0";
+  };
+
+  # Performance
+  services.fstrim.enable = true;
+  services.tuned.enable = true;
+
+  # zRAM Swap
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 50;
+  };
+
+  # File Indexing
+  services.locate = {
+    enable = true;
+    package = pkgs.plocate;
+    interval = "hourly";
+  };
+
+  # Firmware Updates
+  services.fwupd.enable = true;
+
+  # ============================================================================
+  # Desktop Environment & Portal
+  # ============================================================================
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    config.common.default = "*";
+  };
+
+  security.polkit.enable = true;
+
+  # ============================================================================
+  # Programs & Applications
+  # ============================================================================
+
+  # Shell
+  programs.fish.enable = true;
+
+  # Gaming
+  programs.gamemode.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
+
+  # Flatpak
+  services.flatpak = {
+    enable = true;
+    packages = [
+      "org.vinegarhq.Sober"
+    ];
+    update.onActivation = true;
+  };
+
+  # Compatibility
+  programs.nix-ld.enable = true;
+
+  # ============================================================================
+  # System Packages
+  # ============================================================================
+
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+    # Add your packages here
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.tailscale.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
 }
