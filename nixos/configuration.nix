07def025442743c6256debfe1274248cfc8936c9
jeebuscrossaint <thediamond270@gmail.com>
2026-01-08 15:55:30 -0500

    update flake

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -246,12 +246,12 @@
 
   # Gaming
   programs.gamemode.enable = true;
-  # programs.steam = {
-    # enable = true;
-    # remotePlay.openFirewall = true;
-    # dedicatedServer.openFirewall = true;
-    # gamescopeSession.enable = true;
-  # };
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
 
   # Flatpak
   services.flatpak = {
